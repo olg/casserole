2ed16b798b3186452a69ced1c800b6388406020a
Olivier Gutknecht <olg@fotonauts.com>
2009-05-04 23:44:04 +0200

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Cuisine
+//
+//  Created by Olivier Gutknecht on 04/05/09.
+//  Copyright Fotonauts 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
